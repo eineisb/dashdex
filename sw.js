cd7e4756e1dea18ab8562a5516ca4248a15a370a
eineisb <your@email.com>
2026-05-21 11:37:19 +0100

    add PWA with MetaMask deep link

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'dashdex-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE)));
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
