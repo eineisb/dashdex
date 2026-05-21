9dffbfe982d48f4b62d6a7d94fd8c0bea936b085
eineisb <your@email.com>
2026-05-21 11:25:58 +0100

    add PWA support

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'dashdex-v1';
+const ASSETS = ['/dashdex/', '/dashdex/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
